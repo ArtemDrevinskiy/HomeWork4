5c5c3e06588b522a2acaee2f35182930e4718f90
Pavel Belogaenko <ext.pbelogaenko@tinkoff.ru>
2021-08-12 00:54:09 +0300

    Homework 4 review.

--- HomeWork4/HomeWork4/MainViewController.swift
+++ HomeWork4/HomeWork4/MainViewController.swift
@@ -7,12 +7,18 @@
 
 import UIKit
 
+/// У тебя этот контроллер используется сразу для двух представлений в Stodyboard.
+/// Если нужен для обоих - создай новый класс. Если нет - удали из одного.
+
 class MainViewController: UIViewController {
+    
+    /// Не оставляем пустые `override` методы
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
+    /// Параметр `sender` не используется
     @IBAction func thirdPigButtonAction(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: .main)
         let mainWindowController = storyboard.instantiateViewController(withIdentifier: "ThirdPigsHouseWindow")
@@ -20,9 +26,15 @@ class MainViewController: UIViewController {
         present(mainWindowController, animated: true)
     }
     
+    /// Параметр `sender` не используется
     @IBAction func backToMainWindowButtonAction(_ sender: Any) {
         self.dismiss(animated: true)
     }
     
-    
+    /// Не оставляй пустые строки в конце определения
 }
+
+/// По UI:
+/// - лучше использовать кнопку `Back` без стрелки назад, с ней немного сбивает с толку, не тот тип презентации.
+/// - не используй системный синий цвет текста для лейбы, т.к. создается впечетление, что на него можно тапнуть :)
+/// По тематике воспринял буквально, но картинки - огонь!
